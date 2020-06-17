77bf62cfe4b5f21ec3d65264ffe752386a556511
Michael <michaelpp2001@gmail.com>
2020-06-17 01:23:35 -0700

    added delete modal to white wine

--- src/app/white-wine/white-wine.component.ts
+++ src/app/white-wine/white-wine.component.ts
@@ -13,8 +13,23 @@ export class WhiteWineComponent implements OnInit {
     public _wine: WineService,
     public _admin: AdminService
     ) { }
+  
+  wineToDelete: any;
 
   ngOnInit() {
+  }
+
+  showDialog(wine){
+    this.wineToDelete = wine;
+    let modal_t  = document.getElementById('modal_2')
+    modal_t.classList.remove('hhidden')
+    modal_t.classList.add('sshow');
+  }
+
+  closeDialog() {
+      let modal_t  = document.getElementById('modal_2')
+      modal_t.classList.remove('sshow')
+      modal_t.classList.add('hhidden');
   }
 
 }
